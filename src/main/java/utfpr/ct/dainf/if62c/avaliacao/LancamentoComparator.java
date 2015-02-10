1c537e5fbb548bcdafac1cbd058d89591460319e
ithiro <ithiro_sumi@hotmail.com>
2015-02-10 11:54:59 -0200

    Update LancamentoComparator.java

--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/LancamentoComparator.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/LancamentoComparator.java
@@ -6,6 +6,7 @@ package utfpr.ct.dainf.if62c.avaliacao;
  * @author 
  */
 public class LancamentoComparator implements Comparator<Lancamento>{
+    @Override
     public int compare(Lancamento a, Lancamento b){
         if (a.getConta() == b.getConta()) {return a.getData()-b.getData();}
         else {return a.getConta()-b.getConta();}
